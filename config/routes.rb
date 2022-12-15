5c5f1be3aee385c7f046947bf2c5002fbc0704df
Vlad Kostikov <vladkostikovru@gmail.com>
2022-12-16 02:48:34 +0300

    Добавил контроллер contacts и страницу /contacts/new

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get '/' => 'home#index'
 
   resources :articles
+  resource :contacts, only: [:new, :create]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
