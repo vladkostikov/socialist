f57799ebb222f0bee09be6aaeaf8981dcb14e02f
Vlad Kostikov <vladkostikovru@gmail.com>
2022-12-16 03:23:00 +0300

    Сделал миграцию Contact и сохранение в БД данных из формы

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_15_104832) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_15_234907) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "email"
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
