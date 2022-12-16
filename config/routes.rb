f7e410d8e84824287746ad137f56a80531420e05
Vlad Kostikov <vladkostikovru@gmail.com>
2022-12-16 03:51:47 +0300

    Переместил /contacts/new в /contacts

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get '/' => 'home#index'
 
   resources :articles
-  resource :contacts, only: [:new, :create]
+  resource :contacts, only: [:new, :create], path_names: { :new => '' }
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
