62e66c48c31bda124bcf7d3baddc6c2652835650
Vlad Kostikov <vladkostikovru@gmail.com>
2022-12-20 15:16:35 +0300

    Добавил username в БД

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_19_170613) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_20_115823) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
@@ -42,8 +42,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_19_170613) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "comments", "articles"
