eb147d847d6078b8900be9ef5bf55499222eb7e3
Vlad Kostikov <vladkostikovru@gmail.com>
2022-12-24 21:38:25 +0300

    Добавил тест для метода subject

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -9,4 +9,12 @@ describe Article, type: :model do
   describe 'associations' do
     it { should have_many :comments }
   end
+
+  describe '#subject' do
+    it 'returns the article title' do
+      article = create(:article, title: 'Good title')
+
+      expect(article.subject).to eq 'Good title'
+    end
+  end
 end
