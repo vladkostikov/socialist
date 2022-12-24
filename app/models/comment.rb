40aff64d1cdf12dcc2b6837ffff831fce18ba694
Vlad Kostikov <vladkostikovru@gmail.com>
2022-12-24 23:53:19 +0300

    Добавил валидацию максимальной длины комментария

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   validates :author, presence: true
-  validates :body, presence: true
+  validates :body, presence: true, length: { maximum: 4000 }
   belongs_to :article
 end
