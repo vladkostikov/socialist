7e37f018d7e7d5df5903313054e6aadeedd67ddf
Vlad Kostikov <vladkostikovru@gmail.com>
2022-12-25 02:43:24 +0300

    Добавил тесты для проверки длины Title и Text публикации

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 describe Article, type: :model do
   describe 'validations' do
     it { should validate_presence_of :title }
+    it { should validate_length_of(:title).is_at_most(140) }
     it { should validate_presence_of :text }
+    it { should validate_length_of(:text).is_at_most(4000) }
   end
 
   describe 'associations' do
