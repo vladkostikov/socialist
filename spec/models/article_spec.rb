d8caa85db5e207a9c19cb7185c84ee76c75d2503
Vlad Kostikov <vladkostikovru@gmail.com>
2022-12-24 22:11:16 +0300

    Добавил тест для метода last_comment

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -12,9 +12,19 @@ describe Article, type: :model do
 
   describe '#subject' do
     it 'returns the article title' do
+      # Создаём article с указанным title
       article = create(:article, title: 'Good title')
 
       expect(article.subject).to eq 'Good title'
+    end
+  end
+
+  describe '#last_comment' do
+    it 'returns last comment' do
+      # Создаём публикацию с комментариями
+      article = create(:article_with_comments)
+
+      expect(article.last_comment.body).to eq 'Comment body 3'
     end
   end
 end
