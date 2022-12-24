60e8499175868f23c42428cbd6cb7f13fa2047f5
Vlad Kostikov <vladkostikovru@gmail.com>
2022-12-24 22:08:48 +0300

    Обновил factory Article для создания публикации с несколькими комментариями

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,13 @@ FactoryBot.define do
   factory :article do
     title { 'Article title' }
     text { 'Article text' }
+
+    # Создаём фабрику для создания публикации с несколькими комментариями
+    factory :article_with_comments do
+      after :create do |article, evaluator|
+        # Создаём список из 3-х комментариев
+        create_list :comment, 3, article: article
+      end
+    end
   end
 end
