9d91494f20cc7614a894fccf0e101c64cd35bb8c
Vlad Kostikov <vladkostikovru@gmail.com>
2022-12-25 06:50:09 +0300

    Добавил миграцию для связи Article и User

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_20_115823) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_25_030920) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -48,5 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_20_115823) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
 end
