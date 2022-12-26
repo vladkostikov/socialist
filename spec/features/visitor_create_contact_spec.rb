3d30e8b0af29a684a1ce2c8f01b17b712248aec7
Vlad Kostikov <vladkostikovru@gmail.com>
2022-12-27 02:15:01 +0300

    Добавил в тест проверку на отправку обращения

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -5,4 +5,15 @@ feature 'Contact Creation' do
     visit '/contacts'
     expect(page).to have_content I18n.t("contacts.contact_header")
   end
+
+  scenario 'Allows a guest to create contact' do
+    visit '/contacts'
+
+    fill_in :contact_email, with: 'user@example.com'
+    fill_in :contact_message, with: 'Message'
+
+    click_button 'Save Contact'
+
+    expect(page).to have_content 'Спасибо'
+  end
 end
