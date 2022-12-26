b56d108a76f4950c1a3a6cc5854d9c6e06e467be
Vlad Kostikov <vladkostikovru@gmail.com>
2022-12-27 02:53:24 +0300

    Перенёс регистрацию в отдельный метод

--- spec/features/visitor_creates_account_spec.rb
+++ spec/features/visitor_creates_account_spec.rb
@@ -1,16 +1,9 @@
 require 'rails_helper'
+require_relative '../support/session_helper'
 
 feature 'Account creation' do
   scenario 'Allows guest to create account' do
-    visit new_user_registration_path
-
-    fill_in :user_email, with: 'email@email.com'
-    fill_in :user_username, with: 'username'
-    fill_in :user_password, with: 'password'
-    fill_in :user_password_confirmation, with: 'password'
-
-    click_button 'Sign up'
-
+    sign_up
     expect(page).to have_content I18n.t('devise.registrations.signed_up')
   end
 end
---
+++ spec/support/session_helper.rb
@@ -0,0 +1,10 @@
+def sign_up
+  visit new_user_registration_path
+
+  fill_in :user_email, with: 'email@email.com'
+  fill_in :user_username, with: 'username'
+  fill_in :user_password, with: 'password'
+  fill_in :user_password_confirmation, with: 'password'
+
+  click_button 'Sign up'
+end
