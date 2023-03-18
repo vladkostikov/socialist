1cd9655aa6b18f399bb7a85b3c18407f88e925a9
Vlad Kostikov <vladkostikovru@gmail.com>
2023-03-18 16:39:14 +0300

    Рефакторинг

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,21 @@ module Blog
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.default_options = {from: ENV["EMAIL_FROM"]} # example@site.com
     config.action_mailer.default_url_options = { host: ENV["EMAIL_HOST"] } # site.com
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:         'smtp.yandex.ru',
+      port:            465,
+      domain:          'yandex.ru',
+      user_name:       ENV["EMAIL_USERNAME"], # username_email@site.com
+      password:        ENV["EMAIL_PASSWORD"],
+      authentication:  'plain',
+      ssl:             true,
+      tsl:             true,
+      enable_starttls: true,
+      open_timeout:    10,
+      read_timeout:    10 }
 
     config.active_storage.variant_processor = :mini_magick
   end
--- config/environment.rb
+++ config/environment.rb
@@ -3,21 +3,3 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.default_options = { from: ENV["EMAIL_FROM"] } # example@site.com
-ActionMailer::Base.default_url_options = { host: ENV["EMAIL_HOST"] } # site.com
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.raise_delivery_errors = true
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  address:         'smtp.yandex.ru',
-  port:            465,
-  domain:          'yandex.ru',
-  user_name:       ENV["EMAIL_USERNAME"], # username_email@site.com
-  password:        ENV["EMAIL_PASSWORD"],
-  authentication:  'plain',
-  ssl:             true,
-  tsl:             true,
-  enable_starttls: true,
-  open_timeout:    10,
-  read_timeout:    10 }
